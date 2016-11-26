b9026e65c610a809d7e3c5705bd639db07667053
Guy Davenport <daveneti@gmail.com>
2016-11-26 18:50:25 +1300

    added 'results' directory

--- bundles/org.corehunter.ui.services/src/org/corehunter/ui/services/CoreHunterRunServicesBootstraped.java
+++ bundles/org.corehunter.ui.services/src/org/corehunter/ui/services/CoreHunterRunServicesBootstraped.java
@@ -1,12 +1,32 @@
 package org.corehunter.ui.services;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.corehunter.services.simple.SimpleCoreHunterRunServices;
 import org.corehunter.ui.Activator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.osgi.service.datalocation.Location;
 
 public class CoreHunterRunServicesBootstraped extends SimpleCoreHunterRunServices {
 
-    public CoreHunterRunServicesBootstraped() {
+	public static final String RESULTS_DIRECTORY = "results" ;
+
+    public CoreHunterRunServicesBootstraped() throws IOException {
         super(Activator.getDefault().getDatasetServices());
+        
+		Location instanceLocation = Platform.getInstanceLocation();
+		
+		try {
+			Path path = Paths.get(Paths.get(instanceLocation.getURL().toURI()).toString(), RESULTS_DIRECTORY) ;
+			
+			setPath(Files.createDirectories(path));
+		} catch (URISyntaxException e) {
+			throw new IOException("Can not create datasets directory", e) ;
+		}
     }
 
 }
