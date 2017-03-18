209407abb2533dceff7736d7849dd9979ff4344b
Guy Davenport <daveneti@gmail.com>
2017-03-18 18:27:46 +1300

    added the version and build number to the about message

--- bundles/org.corehunter.ui/src/org/corehunter/ui/AboutHandler.java
+++ bundles/org.corehunter.ui/src/org/corehunter/ui/AboutHandler.java
@@ -18,6 +18,7 @@ package org.corehunter.ui;
 
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -28,7 +29,10 @@ public class AboutHandler {
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell parentShell) {
 		MessageDialog.openInformation(parentShell, 
 				"About Core Hunter",
-				"Core Hunter user interface\n" 
+				
+				"Core Hunter User Interface" 
+						+ " Version : " + getGUIVersion() + "\n"
+						+ "\tRunning version : " + getCoreHunterVersion() + " of CoreHunter \n\n"
 						+ "(c) Copyright Guy Davenport and Herman De Beukelaer 2009-2017.\n" 
 						+ "All rights reserved.\n\n"
 						+ "This product includes software developed by other open source projects\n"
@@ -36,5 +40,35 @@ public class AboutHandler {
 						+ "and Apache Software Foundation, https://www.apache.org/.\n" 
 						+ "For more details see http://www.corehunter.org");
 	}
+	
+	private String getVersion(String version) {
+
+		String[] split = version.split("\\.") ;
 		
+		switch (split.length) {
+			case 0 : 
+				return "Unknown!" ;
+			case 1 : 
+				return String.format("%s.0.0 (build : unknown)", split[0]) ;
+			case 2 : 
+				return String.format("%s.%s.0 (build : unknown)", split[0], split[1]) ;
+			case 3 : 
+				return String.format("%s.%s.%s (build : unknown)", split[0], split[1], split[2]) ;
+			default :
+			case 4 : 
+				if  (split[3] == null || split[3].trim().isEmpty() || "qualifier".equals(split[3])) {
+					return String.format("%s.%s.%s (build : unknown)", split[0], split[1], split[2]) ;
+				} else {
+					return String.format("%s.%s.%s (build : %s)", split[0], split[1], split[2], split[3]) ; 
+				}
+		}
+	}
+	
+	private String getGUIVersion() {
+		return getVersion(Platform.getProduct().getDefiningBundle().getVersion().toString());
+	}
+	
+	private String getCoreHunterVersion() {
+		return getVersion(Platform.getBundle("org.corehunter.base").getHeaders().get("Bundle-Version"));
+	}	
 }
